49b319afc012caf5e14508766240ea0b9bf0e867
Lenadro Silva Lima <leandroslima09@gmail.com>
2025-09-22 20:01:35 +0000

    codigo interativo

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -5,31 +5,47 @@ class Towel:
         self.size = size
         self.wetness = 0  
 
+    def getMaxWetness(self):
+        if self.size == "P":
+            return 10
+
+        elif self.size == "M":
+            return 20
+
+        elif self.size == "G":
+            return 30
+        return 0
+
     def dry (self, amount:int) -> None:
         self.wetness =+ amount
         if self.wetness >= self.getMAxetness():
             print("toalha encharcada")
             self.wetness = self.getMaxWetness()
 
-    def isDry (self) -> bool:
-        return self.wetness == 0
-
     def WringOut(self) -> None:
         self.wetness == 0
 
-    def __str__(self) -> str: 
+    def isDry (self) -> bool:
+        return self.wetness == 0
 
+    def __str__(self) -> str: 
         return f"cor:{self.color}, tam:{self.size}, hum:{self.wetness}"
 
-doguito = Towel ("Encardida","G")
-doguito.size = "M"
-print(doguito) 
-doguito,dry(3)
-print(doguito)
-doguito.dry(15)
-doguito.dry(10)
-print(doguito)
-print(doguito.isDry())
-
-
-
+def main ():
+    toalha = Towel("", "")
+    while True:
+        line: str = input()
+        agrs: list[str] = line.split(" ")
+        if args [0] == "end":
+            break
+        elif args[0] == "new":
+            color = args[1]
+            size = agrs[2]
+            toalha = Towel(color,size)
+        elif args[0] == "show":
+            print(toalha)
+        elif args[0] == "dry":
+            amount: int = int(args[1])
+            toalha.dry(amount)
+        else:
+            print("fail: comando nao identificado")
