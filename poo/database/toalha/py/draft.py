8c1b5be0e211e37062df4239242fc7c85d61c2e8
Lenadro Silva Lima <leandroslima09@gmail.com>
2025-09-22 19:23:15 +0000

    doguito incompleto

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -3,13 +3,33 @@ class Towel:
 
         self.color = color
         self.size = size
-        self.wetness = 0        
-    def __str__(self): 
+        self.wetness = 0  
+
+    def dry (self, amount:int) -> None:
+        self.wetness =+ amount
+        if self.wetness >= self.getMAxetness():
+            print("toalha encharcada")
+            self.wetness = self.getMaxWetness()
+
+    def isDry (self) -> bool:
+        return self.wetness == 0
+
+    def WringOut(self) -> None:
+        self.wetness == 0
+
+    def __str__(self) -> str: 
 
         return f"cor:{self.color}, tam:{self.size}, hum:{self.wetness}"
 
-towel = Towel("green","g")
-towel1 = Towel("black","p")
+doguito = Towel ("Encardida","G")
+doguito.size = "M"
+print(doguito) 
+doguito,dry(3)
+print(doguito)
+doguito.dry(15)
+doguito.dry(10)
+print(doguito)
+print(doguito.isDry())
 
 
 
